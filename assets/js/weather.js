e82b885158952fdf0aa30da0b5b63f8e92ac7aeb
vijaylad12 <ladv728@gmail.com>
2024-08-31 14:54:36 +0530

    weather responsive

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -1,3 +1,62 @@
+/*=============== SHOW MENU ===============*/
+
+const navMenu=document.getElementById('nav-menu'),
+      navToggle=  document.getElementById('nav-toggle'),
+      navClose=document.getElementById('nav-close')
+
+// validate if toggle exits
+
+if(navToggle){
+    navToggle.addEventListener('click',()=>{
+        navMenu.classList.add('show-menu');
+    })
+} 
+
+// validate if constant exits
+
+if(navClose){
+    navClose.addEventListener('click',()=>{
+        navMenu.classList.remove('show-menu');
+    })
+}
+
+
+/*=============== REMOVE MENU MOBILE ===============*/
+const navLink=document.querySelectorAll('.nav__link')
+
+const linkAction=()=>{
+    const navMenu=document.getElementById('nav-menu')
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n=>n.addEventListener('click',linkAction))
+/*=============== ADD BLUR TO HEADER ===============*/
+
+const blurHeader=()=>{
+    const header=document.getElementById('header')
+    this.scrollY >= 50 ? header.classList.add('blur-header'):header.classList.remove('blur-header')
+}
+window.addEventListener('scroll',blurHeader)
+/*=============== SHOW SCROLL UP ===============*/ 
+const scrollUp = () =>{
+    const scrollUp=document.getElementById('scroll-up')
+
+    this.scrollY >= 350 ? scrollUp.classList.add('show-scroll'):scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll',scrollUp)
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr=ScrollReveal({
+    origin:'top',
+    distance:'60px',
+    duration:3000,
+    delay:400,
+
+})
+
+//weather
+
 let country = document.querySelector("#country");
 let city = document.querySelector("#city");
 let check = document.querySelector("#check");
